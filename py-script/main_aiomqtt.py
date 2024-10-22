db710e4066cda788499408ca203a765aaf687919
AmozDieleman <amoz.dieleman@gmail.com>
2024-10-22 17:31:16 +0200

    added cmd_handler, errors to be fixed

--- py-script/main_aiomqtt.py
+++ py-script/main_aiomqtt.py
@@ -3,7 +3,6 @@ import aiomqtt
 import json
 
 
-
 vars_dict = {
     "evse_id": "NL*DCO*E1234*1",
     "ready": False,  # Placeholder for boolean; update as needed
@@ -37,22 +36,22 @@ vars_dict = {
         "connector_type": "IEC62196Type2Cable"
     },
     "ev_info": {  # Optional field, include only if available
-        "soc": "%",
-        "present_voltage": "V",
-        "present_current": "A",
-        "target_voltage": "V",
-        "target_current": "A",
-        "maximum_current_limit": "A",
-        "minimum_current_limit": "A",
-        "maximum_voltage_limit": "V",
-        "minimum_voltage_limit": "V",
+        "soc": 0,
+        "present_voltage": 0.0,
+        "present_current": 0.0,
+        "target_voltage": 0.0,
+        "target_current": 0.0,
+        "maximum_current_limit": 0.0,
+        "minimum_current_limit": 0.0,
+        "maximum_voltage_limit": 0.0,
+        "minimum_voltage_limit": 0.0,
         "estimated_time_full": "date-time",
         "departure_time": "date-time",
         "estimated_time_bulk": "date-time",
         "evcc_id": "MAC-address",
-        "remaining_energy_needed": "Wh",
-        "battery_capacity": "Wh",
-        "battery_full_soc": "%",
+        "remaining_energy_needed": 0.0,
+        "battery_capacity": 0.0,
+        "battery_full_soc": 100,
         "battery_bulk_soc": 80
     },
     "telemetry": {
@@ -76,23 +75,116 @@ vars_dict = {
         "power_W": 0.0     # Placeholder for float; update as needed, optional
     }
 }
-
 vars_dict_key_list = list(vars_dict.keys())
+
+cmds_dict = {
+    "get_evse":{
+        "result":{
+            "id":1,
+            "connectors":[[1,"cCCS2"]]
+        }
+    },
+
+    "enable_disable":{
+        "connector_id":"",
+        "cmd_source":"insert source",
+        # Add function to check status EVSE after command, result is true when turned on after cmd and false if turned off after cmd. 
+        "result":True
+    },
+
+    "authorize_response":{
+        "provided_token":"insert token",
+        "validation_result":"insert result"
+    },
+    
+    # No args or result, cannot start transaction after call.
+    "withdraw_authorization":"don't start transaction",
+
+    # Call to signal that EVSE is reserved, return True if reservation is accepted, false if rejected.
+    "reserve":{
+        "reservation_id":"",
+        "result":False
+    },
+
+    # No args or result, EVSE no longer reserved after call.
+    "cancel_reservation":"cancel_reservation()",
+
+    # No args or result, set EVSE to faulted externally after call.
+    "set_faulted":"set_evse_faulted()",
+
+    # No args, pause charging, return True if charging paused after call, False otherwise.
+    "pause_charging":{
+        "result":False
+    },
+
+    # No args, resume charging, return True if charging resumed after call, False otherwise.
+    "resume_charging":{
+        "result": False
+    },
+
+    # Request to stop transaction, return True if succesful.
+    # "stop_transaction":{
+    #     "request"{
+
+    #     },
+    #     "result":False
+    # },
+
+    # Forces connector to unlock immediately, return True if succesfully unlocked, False otherwise.
+    "force_unlock":{
+        "connector_id":1,
+        "result":False
+    },
+
+    # No result, set energy limits at specified node
+    # "set_external_limits":{
+    #     "value" -> object:{
+
+    #     }
+    # },
+
+    # No result, set Contract Certificate
+    # "set_get_certificate_response":{
+    #     "certificate_response" -> object:{
+
+    #     }
+    # },
+
+    # No args, return True if call was used by EVSE
+    "external_ready_to_start_charging":{
+        "result":False
+    }
+}
+cmds_dict_key_list = list(cmds_dict.keys())
+
 
 broker = "192.168.192.150"
 
-var_standard = {'data':'insert_data_object','name':'insert_var_name'}
-cmd_standard = {}
+var_standard = {"data":"insert_data_object","name":"insert_var_name"}
+cmd_standard = {"data":{"id":"insert id from call","origin":"evse_manager","retval":"insert return value"},"name":"insert name of call","type":"result"}
 
 topic_var = "everest/evse_manager/evse/var"
 topic_cmd = "everest/evse_manager/evse/cmd"
 
 
-async def publish_var_json(client, var=0):
+def cmd_handler(cmd, client):
+    if cmd["type"] == "call":
+        cmd_standard["data"]["id"] = cmd["data"]["id"]
+        cmd_standard["data"]["retval"] = cmds_dict[cmd["name"]]["result"]
+        cmd_standard["name"] = cmd["name"]
+        msg = json.dumps(var_standard, separators=(",",":"))
+        
+        return msg
+
+    else:
+        pass
+
+
+async def publish_json(client, var=0):
     while True:
         for var in vars_dict_key_list:
             await asyncio.sleep(0.2)
-            var_standard['data'], var_standard['name'] = vars_dict[var], var
+            var_standard["data"], var_standard["name"] = vars_dict[var], var
             msg = json.dumps(var_standard, separators=(",",":"))
             await client.publish(topic_var, payload=msg)
 
@@ -101,6 +193,8 @@ async def read_cmd_to_dict(client):
     await client.subscribe(topic_cmd)
     async for message in client.messages:
         print(json.loads(message.payload))
+        msg = cmd_handler(json.loads(message.payload))
+        await client.publish(topic_cmd, payload=msg)
 
 
 async def main():
